21db6607c031f0868a957580fad43c484b2cccc0
Jake Reck (Experis) <v-jareck@microsoft.com>
2019-08-27 10:35:20 -0700

    Added reducers and actions

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,26 @@ import Home from './components/layout/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={ Home } />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={ Register } />
-          <Route exact path="/login" component={ Login } />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={ Home } />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
